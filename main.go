de5103672f32796dd6ed57c00e3737ae6bfcd3f7
Aleksandr Komarov <aleksandr.komarov@nordigy.ru>
2024-12-10 00:38:25 +0100

    day09

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"AdventOfCode2024/day08"
+	"AdventOfCode2024/day09"
 	"AdventOfCode2024/utils"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Answer:", day08.Part2(utils.ReadFileLines("day08/input.txt")))
+	fmt.Println("Answer:", day09.Part2(utils.ReadFileLines("day09/input.txt")))
 }
